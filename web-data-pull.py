64b05a313834670f78527e1a9bc58f9ab601e25f
KJ <kjk@skyhopper.net>
2025-03-15 14:19:39 -0400

    future file to be implemented

---
+++ web-data-pull.py
@@ -0,0 +1,5 @@
+# TODO: on run, scrapes data off blue alliance to supplement locally stored info
+# When? When wifi is available at comps or momentary times of cellular
+# example: 
+# downloads match info, stores into team scouting data .json files, 
+# overwrites any existing match data (except for tags) as it's more accurate
