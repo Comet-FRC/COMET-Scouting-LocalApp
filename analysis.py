77bb72633cced25ebc8d91c23f7d8dcf2f0a9e1e
KJ <kjk@skyhopper.net>
2025-03-15 12:18:40 -0400

    common tags, common end pos console visualization

--- analysis.py
+++ analysis.py
@@ -8,13 +8,13 @@ stats = pd.read_json('team_stats.json', orient='index')
 # Select a column to visualize
 stat_to_plot = ""
 while stat_to_plot == "" or not (stat_to_plot in stats.columns):
-    print(f"'{stat_to_plot}'")
     stat_to_plot = input("Enter a column name to visualize: ")
 
 if "Common" not in stat_to_plot:
     # Plot bar chart
     plt.figure(figsize=(10, 5))
     stats[stat_to_plot].plot(kind="bar")
+
     # Labeling
     plt.xlabel("Teams")
     plt.ylabel(stat_to_plot)
@@ -22,6 +22,8 @@ if "Common" not in stat_to_plot:
     plt.xticks(rotation=45)
     plt.grid(axis="y", linestyle="--", alpha=0.7)
 else:
-    # implement string val plotting
-    pass
+    for i in stats[stat_to_plot].index:
+        print(f"{i}:", stats[stat_to_plot].loc[i])
+
+    
 plt.show()
