829d3794d2e971b18e47d77e050440625560f8eb
KJ <kjk@skyhopper.net>
2025-03-15 07:21:18 -0400

    action items

--- analysis.py
+++ analysis.py
@@ -19,7 +19,7 @@ for filename in os.listdir(data_dir):
     i_columns_to_avg = [0, 1, 2, 3, 4, 5, 6, 12, 13, 14]
     columns_to_avg = team_data.iloc[:, i_columns_to_avg]
 
-    # handle list values in columns 1-6 (inclusive) by converting list-types to their length values
+    # handle list values in columns 1-6 (inclusive) by converting list-types to their length 
     for col in columns_to_avg.columns:
         if columns_to_avg[col].apply(lambda x: isinstance(x, list)).any(): # checks if any element is a list in the column 
             columns_to_avg[col] = columns_to_avg[col].apply(lambda x: len(x) if isinstance(x, list) else x) # if so, 
@@ -28,15 +28,20 @@ for filename in os.listdir(data_dir):
     avg_values = columns_to_avg.mean().values
     new_names = [f"Average {col}" for col in columns_to_avg.columns]
 
+    #TODO: Most Common Strength, Most Common Weakness
+
     # create a DataFrame with the team's averaged stats
     teamData = pd.DataFrame([avg_values], index=[filename[:-5]], columns=new_names)
 
-    # append to main stats dataframe
+    # append to main stats DataFrame
     stats = pd.concat([stats, teamData], ignore_index=False)
 
 # print resulting DataFrame
 print(stats.head())
+# print(stats["Average Processor"].head())
 
 # save to JSON for further processing
 stats.to_json("team_stats.json", orient="index")
+
+#TODO: Matplotlib analysis here
 
