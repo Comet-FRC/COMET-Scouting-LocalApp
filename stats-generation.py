fixed common tags appending to average values error on stat gen

stats-generation.py
@@ -22,28 +22,33 @@ for filename in os.listdir(data_dir):
     # handle list values in columns 1-6 (inclusive) by converting list-types to their length 
     for col in columns_to_avg.columns:
         if columns_to_avg[col].apply(lambda x: isinstance(x, list)).any(): # checks if any element is a list in the column 
-            columns_to_avg[col] = columns_to_avg[col].apply(lambda x: len(x) if isinstance(x, list) else x) # if so, 
+            columns_to_avg.loc[:, col] = columns_to_avg[col].apply(lambda x: len(x) if isinstance(x, list) else x) # if so, 
 
     # compute mean values for each column
-    avg_values = columns_to_avg.mean().values
+    avg_values = columns_to_avg.mean().values.tolist()
     new_names = [f"Average {col}" for col in columns_to_avg.columns]
+    # print(len(avg_values))
+    # print(len(new_names))
 
     # getting two most common tags
     new_names.append("Common Tags")
     twoCommonTags = team_data["Tags"].mode()[:2]
-    avg_values = np.append(avg_values, twoCommonTags)
+    avg_values.append(twoCommonTags)
+    '''for x in avg_values:
+        if isinstance(x, list):
+            print("x")
+        print(f"{x}, ", end="")'''
 
     # average end position
     new_names.append("Common End Position")
     end_pos = team_data["End Position"].mode()[0]
-    avg_values = np.append(avg_values, end_pos)
+    avg_values.append(end_pos)
      
     # get l1, l2, l3, l4 data out of team_data since columns_to_avg contains transformed data
     coral_points = 0
     for col in team_data.iloc[:, [0, 1, 2, 3]].columns:
         for i in range(len(team_data[col])):
             values = team_data[col].iloc[i]
-            print(values)
             # based on timestamp and auton timestamp, determine which were scored during auton
             for val in values:
                 if val < team_data["Auton Ended"].iloc[i]:
@@ -59,9 +64,8 @@ for filename in os.listdir(data_dir):
 
     # add coral auton point total to names and avg_values
     new_names.append("Average Coral Auton Points")
-    avg_values = np.append(avg_values, coral_points)
+    avg_values.append(coral_points)
 
-    # create a DataFrame with the team's averaged stats
     teamData = pd.DataFrame([avg_values], index=[filename[:-5]], columns=new_names)
 
     # append to main stats DataFrame