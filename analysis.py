8fce16145f645231d0d6956d546bad88b1f762cd
KJ <kjk@skyhopper.net>
2025-03-15 10:09:21 -0400

    average coral points and other stats added

--- analysis.py
+++ analysis.py
@@ -28,7 +28,38 @@ for filename in os.listdir(data_dir):
     avg_values = columns_to_avg.mean().values
     new_names = [f"Average {col}" for col in columns_to_avg.columns]
 
-    #TODO: Most Common Strength, Most Common Weakness
+    # getting two most common tags
+    new_names.append("Average Tags")
+    twoCommonTags = team_data["Tags"].mode()[:2]
+    avg_values = np.append(avg_values, twoCommonTags)
+
+    # average end position
+    new_names.append("Average End Position")
+    end_pos = team_data["End Position"].mode()[0]
+    avg_values = np.append(avg_values, end_pos)
+     
+    # get l1, l2, l3, l4 data out of team_data since columns_to_avg contains transformed data
+    coral_points = 0
+    for col in team_data.iloc[:, [0, 1, 2, 3]].columns:
+        for i in range(len(team_data[col])):
+            values = team_data[col].iloc[i]
+            print(values)
+            # based on timestamp and auton timestamp, determine which were scored during auton
+            for val in values:
+                if val < team_data["Auton Ended"].iloc[i]:
+                    # add up to point total (based on coral level)
+                    if col == "L1":
+                        coral_points += 3
+                    elif col == "L2":
+                        coral_points += 4
+                    elif col == "L3":
+                        coral_points += 6
+                    else:
+                        coral_points += 7
+
+    # add coral auton point total to names and avg_values
+    new_names.append("Average Coral Auton Points")
+    avg_values = np.append(avg_values, coral_points)
 
     # create a DataFrame with the team's averaged stats
     teamData = pd.DataFrame([avg_values], index=[filename[:-5]], columns=new_names)
@@ -41,7 +72,9 @@ print(stats.head())
 # print(stats["Average Processor"].head())
 
 # save to JSON for further processing
-stats.to_json("team_stats.json", orient="index")
+stats.to_json("team_stats.json", orient="index", indent=2)
 
 #TODO: Matplotlib analysis here
+import matplotlib
+
 
